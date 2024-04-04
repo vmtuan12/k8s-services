5239e95d14b4d424ce91f316fbac56f2fa8700a6
vmtuan12 <minhtuan3154@gmail.com>
2024-04-04 23:24:55 +0700

    multi broker

--- kafka_prod.py
+++ kafka_prod.py
@@ -2,9 +2,9 @@ from kafka import KafkaProducer
 import json
 import datetime
 
-producer = KafkaProducer(bootstrap_servers=['192.168.56.63:30007'], 
+producer = KafkaProducer(bootstrap_servers=['localhost:9093'], 
                         value_serializer=lambda x: json.dumps(x).encode('utf-8'))
-topic = 'k8s_1'
+topic = 'my-topic-three'
 
 msg = {
     "time": str(datetime.datetime.now())
