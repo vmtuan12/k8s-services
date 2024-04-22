563b32763f3efc92c406a63cf1f7536f9c5b063d
vmtuan12 <minhtuan3154@gmail.com>
2024-04-22 14:25:41 +0700

    dag 5 modify

--- airflow/dags/example_dag_5.py
+++ airflow/dags/example_dag_5.py
@@ -5,9 +5,9 @@ from datetime import datetime
 # Define the DAG
 dag = DAG(
     'example_5_new',
-    description='example 5 new description',
+    description='example 5 new description 2',
     schedule_interval=None,
-    start_date=datetime(2023, 3, 23),
+    start_date=datetime(2023, 3, 24),
     catchup=False
 )
 
