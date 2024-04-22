f1a375be10fbfae5b4eabcc09c3ebcf7c9f0d6da
vmtuan12 <minhtuan3154@gmail.com>
2024-04-22 11:26:26 +0700

    example dag 2

---
+++ airflow/dags/example_dag_2.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+# Define the DAG
+dag = DAG(
+    'example_2',
+    description='A simple tutorial DAG',
+    schedule_interval=None,
+    start_date=datetime(2023, 3, 22),
+    catchup=False
+)
+
+# Define the BashOperator task
+hello_world_task = BashOperator(
+    task_id='hello_world_task',
+    bash_command='python -c "print(\'Hello, world!\')"',
+    dag=dag
+)
+
+# Define the task dependencies
+hello_world_task
